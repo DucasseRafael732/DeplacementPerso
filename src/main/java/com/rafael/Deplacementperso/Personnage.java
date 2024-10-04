4271a3ea764ef91cadad1be60b273fbc3b60dd36
DucasseRafael <rafael404651@gmail.com>
2024-10-04 09:02:49 +0200

    commit 3

--- src/main/java/com/rafael/Deplacementperso/Personnage.java
+++ src/main/java/com/rafael/Deplacementperso/Personnage.java
@@ -2,22 +2,50 @@ package com.rafael.Deplacementperso;
 
 public class Personnage {
 
-    public Orientation tourner(int nbre) {
+    public Orientation tourner(int nbre, Orientation orientation) {
 
         nbre = nbre % 4;
 
-        if (nbre < 0) {
-            nbre += 4;
-        }
-
-        if (nbre == 0) {
-            return Orientation.NORD;
-        } else if (nbre == 1) {
-            return Orientation.EST;
-        } else if (nbre == 2) {
-            return Orientation.SUD;
-        } else if (nbre == 3) {
-            return Orientation.OUEST;
+        if (orientation == Orientation.NORD) {
+            if (nbre == 0) {
+                return Orientation.NORD;
+            } else if (nbre == 1) {
+                return Orientation.EST;
+            } else if (nbre == 2) {
+                return Orientation.SUD;
+            } else if (nbre == 3) {
+                return Orientation.OUEST;
+            }
+        } else if (orientation == Orientation.EST) {
+            if (nbre == 0) {
+                return Orientation.EST;
+            } else if (nbre == 1) {
+                return Orientation.SUD;
+            } else if (nbre == 2) {
+                return Orientation.OUEST;
+            } else if (nbre == 3) {
+                return Orientation.NORD;
+            }
+        } else if (orientation == Orientation.SUD) {
+            if (nbre == 0) {
+                return Orientation.SUD;
+            } else if (nbre == 1) {
+                return Orientation.OUEST;
+            } else if (nbre == 2) {
+                return Orientation.NORD;
+            } else if (nbre == 3) {
+                return Orientation.EST;
+            }
+        } else if (orientation == Orientation.OUEST) {
+            if (nbre == 0) {
+                return Orientation.OUEST;
+            } else if (nbre == 1) {
+                return Orientation.NORD;
+            } else if (nbre == 2) {
+                return Orientation.EST;
+            } else if (nbre == 3) {
+                return Orientation.SUD;
+            }
         }
 
         return null;
--- src/test/java/com/rafael/Deplacementperso/PersonnageTest.java
+++ src/test/java/com/rafael/Deplacementperso/PersonnageTest.java
@@ -12,7 +12,7 @@ class PersonnageTest {
     void quand_orientation_NORD_tourner_1_devrait_orienter_vers_EST(){
         Personnage avatar = new Personnage();
 
-        Orientation actualOrientation =avatar.tourner(1);
+        Orientation actualOrientation =avatar.tourner(1,Orientation.NORD);
 
         assertThat(actualOrientation).isEqualTo(Orientation.EST);
     }
@@ -21,7 +21,7 @@ class PersonnageTest {
     void quand_orientation_NORD_tourner_2_devrait_orienter_vers_SUD(){
         Personnage avatar = new Personnage();
 
-        Orientation actualOrientation =avatar.tourner(2);
+        Orientation actualOrientation =avatar.tourner(2,Orientation.NORD);
 
         assertThat(actualOrientation).isEqualTo(Orientation.SUD);
     }
@@ -30,7 +30,7 @@ class PersonnageTest {
     void quand_orientation_NORD_tourner_3_devrait_orienter_vers_OUEST(){
         Personnage avatar = new Personnage();
 
-        Orientation actualOrientation =avatar.tourner(3);
+        Orientation actualOrientation =avatar.tourner(3,Orientation.NORD);
 
         assertThat(actualOrientation).isEqualTo(Orientation.OUEST);
     }
@@ -39,20 +39,19 @@ class PersonnageTest {
     void quand_orientation_NORD_tourner_4_devrait_orienter_vers_NORD(){
         Personnage avatar = new Personnage();
 
-        Orientation actualOrientation =avatar.tourner(4);
+        Orientation actualOrientation =avatar.tourner(4,Orientation.NORD);
 
         assertThat(actualOrientation).isEqualTo(Orientation.NORD);
     }
 
+
     @Test
-    void quand_orientation_NORD_tourner_5_devrait_orienter_vers_EST(){
+    void quand_orientation_OUEST_tourner_1_devrait_orienter_vers_NORD(){
         Personnage avatar = new Personnage();
 
-        Orientation actualOrientation =avatar.tourner(5);
+        Orientation actualOrientation =avatar.tourner(1,Orientation.OUEST);
 
-        assertThat(actualOrientation).isEqualTo(Orientation.EST);
+        assertThat(actualOrientation).isEqualTo(Orientation.NORD);
     }
-
-
 
 }
